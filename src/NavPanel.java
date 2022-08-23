359756ca63417b9da6be19695e2fad79d23d1340
Marco Haderk <m.haderk@gmail.com>
2022-08-23 15:37:23 +0200

    Replace the navbar-panels by buttons. Look-and-feel as mainPnl.

--- src/NavPanel.java
+++ src/NavPanel.java
@@ -1,18 +1,28 @@
+import javax.swing.*;
 import java.awt.Color;
 
-import javax.swing.*;
-
 public class NavPanel extends JPanel {
-    private JPanel left = new JPanel();
-    private JPanel right = new JPanel();
+    private JButton calculationBtn = new JButton("Calculate");
+    private JButton configBtn = new JButton("Config");
     
     public NavPanel() {
         this.setLayout( new BoxLayout(this, BoxLayout.X_AXIS) );
         this.setMaximumSize( new java.awt.Dimension(500, 50) );
 
-        left.setBackground(Color.BLUE);
-        this.add(left);
-        right.setBackground(Color.YELLOW);
-        this.add(right);
+        calculationBtn.setMinimumSize( new java.awt.Dimension(75, 50) );
+        calculationBtn.setPreferredSize( new java.awt.Dimension(110, 50) );
+        calculationBtn.setMaximumSize( new java.awt.Dimension(250, 50) );
+        calculationBtn.setBackground(new Color(238, 238, 238));
+        calculationBtn.setBorderPainted(false);
+        calculationBtn.setFocusPainted(false);
+        this.add(calculationBtn);
+
+        configBtn.setMinimumSize( new java.awt.Dimension(75, 50) );
+        configBtn.setPreferredSize( new java.awt.Dimension(110, 50) );
+        configBtn.setMaximumSize( new java.awt.Dimension(250, 50) );
+        configBtn.setBackground(new Color(238, 238, 238));
+        configBtn.setBorderPainted(false);
+        configBtn.setFocusPainted(false);
+        this.add(configBtn);
     }
 }
