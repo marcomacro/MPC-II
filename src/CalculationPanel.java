3a8fdb5fee5d0834dc29b7910a1aaf51f07e48c2
Marco Haderk <m.haderk@gmail.com>
2022-09-02 10:11:48 +0200

    Display price of choosen material on CalculationPanel.

--- src/CalculationPanel.java
+++ src/CalculationPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.JPanel;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import java.util.SortedMap;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +9,7 @@ public class CalculationPanel extends JPanel {
 
     private Window mainWindow;
     private JComboBox<String> materialCmbBox;
+    private JLabel choosenPriceLbl;
     private DataInputPanel heightPnl;
     private DataInputPanel widthPnl;
     private DataOutputPanel resultPnl = new  DataOutputPanel("Price");
@@ -53,11 +55,15 @@ public class CalculationPanel extends JPanel {
             }
         });
 
+        choosenPriceLbl = new JLabel();
+
         this.setLayout( new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS) );
         this.setMaximumSize( new java.awt.Dimension(500, 800) );
 
         this.add(javax.swing.Box.createRigidArea( new java.awt.Dimension(0, 25) ));
         this.add(materialCmbBox);
+        this.add(javax.swing.Box.createRigidArea( new java.awt.Dimension(0, 5) ));
+        this.add(choosenPriceLbl);
         this.add(javax.swing.Box.createRigidArea( new java.awt.Dimension(0, 35) ));
         this.add(heightPnl);
         this.add(javax.swing.Box.createRigidArea( new java.awt.Dimension(0, 15) ));
@@ -77,6 +83,7 @@ public class CalculationPanel extends JPanel {
         Float p = mainWindow.materialLst.get(materialCmbBox.getSelectedItem());
         Float h = heightPnl.getValue();
         Float w = widthPnl.getValue();
+        choosenPriceLbl.setText(String.format(java.util.Locale.US ,"%,.2f", p));
         resultPnl.setResult(String.format(java.util.Locale.US ,"%,.2f", h * w * p));
     }
 
