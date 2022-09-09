d3d289a8eae1c50f331ded0486474e619e68695b
Marco Haderk <m.haderk@gmail.com>
2022-09-09 10:42:09 +0200

    Rearranged NavPanel.java.

--- src/NavPanel.java
+++ src/NavPanel.java
@@ -1,42 +1,52 @@
+import java.awt.Dimension;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class NavPanel extends JPanel implements ActionListener {
+public class NavPanel extends JPanel implements java.awt.event.ActionListener {
 
     private Window mainWindow;
-    private JButton calculationBtn = new JButton("Calculate");
-    private JButton configBtn = new JButton("Config");
+    
+    private JButton calculationBtn;
+    private JButton configBtn;
     
     public NavPanel(Window w) {
         mainWindow = w;
 
         this.setLayout( new BoxLayout(this, BoxLayout.X_AXIS) );
-        this.setMaximumSize( new java.awt.Dimension(500, 50) );
+        this.setMaximumSize( new Dimension(500, 50) );
 
-        calculationBtn.setMinimumSize( new java.awt.Dimension(75, 50) );
-        calculationBtn.setPreferredSize( new java.awt.Dimension(110, 50) );
-        calculationBtn.setMaximumSize( new java.awt.Dimension(250, 50) );
+        // set up CalculationBtn
+        calculationBtn = new JButton("Calculate");
+
+        calculationBtn.setMinimumSize( new Dimension(75, 50) );
+        calculationBtn.setPreferredSize( new Dimension(110, 50) );
+        calculationBtn.setMaximumSize( new Dimension(250, 50) );
+
         calculationBtn.setBackground(mainWindow.getBaseColor());
         calculationBtn.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, mainWindow.getEmphasizedColor()));
-        // calculationBtn.setBorderPainted(false);
         calculationBtn.setFocusPainted(false);
+        
         calculationBtn.addActionListener(this);
         this.add(calculationBtn);
 
-        configBtn.setMinimumSize( new java.awt.Dimension(75, 50) );
-        configBtn.setPreferredSize( new java.awt.Dimension(110, 50) );
-        configBtn.setMaximumSize( new java.awt.Dimension(250, 50) );
+        // set up ConfigBtn
+        configBtn  = new JButton("Config");
+
+        configBtn.setMinimumSize( new Dimension(75, 50) );
+        configBtn.setPreferredSize( new Dimension(110, 50) );
+        configBtn.setMaximumSize( new Dimension(250, 50) );
+
         configBtn.setBackground(mainWindow.getBaseColor());
         configBtn.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, mainWindow.getEmphasizedColor()));
         configBtn.setBorderPainted(false);
         configBtn.setFocusPainted(false);
+
         configBtn.addActionListener(this);
         this.add(configBtn);
     }
 
-    public void actionPerformed(ActionEvent ae) {
+    public void actionPerformed(java.awt.event.ActionEvent ae) {
         mainWindow.switchView(ae.getActionCommand());
+        // update NavPanel appearance
         if (ae.getActionCommand().equals("Calculate")) {
             calculationBtn.setBorderPainted(true);
             configBtn.setBorderPainted(false);
@@ -44,8 +54,6 @@ public class NavPanel extends JPanel implements ActionListener {
         else if (ae.getActionCommand().equals("Config")) {
             calculationBtn.setBorderPainted(false);
             configBtn.setBorderPainted(true);
-        } else {
-            System.out.println("ERROR: Clicked button not recognized.");
         }
     }
 
